80a1218b22999783b83bd4c6838a6235e6882eff
yMarceloMaia <marcelomaiajp@gmail.com>
2023-11-25 10:22:36 -0300

    starting

---
+++ src/recorder.py
@@ -0,0 +1,41 @@
+import pyaudio
+import wave
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    format=FORMAT, 
+    channels=CHANNELS, 
+    rate=RATE, 
+    input=True, 
+    frames_per_buffer=CHUNK
+)
+
+frames = []
+
+try:
+    while True:
+        data = stream.read(CHUNK)
+        frames.append(data)
+except KeyboardInterrupt:
+    pass
+
+print(frames)
+
+print("recording stopped")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open("output.wav", "wb")
+wf.setnchannels(CHANNELS),
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b"".join(frames))
+wf.close()
