44900e08880c61bd80185ec750ac9b323dd81006
yuzo <yuzokamoto@gmail.com>
2023-12-01 16:38:02 -0300

    tts implementado

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from recorder import recorder
 from transcribe import transcribe
 from translate import translater
+from text_to_speech import text_to_speech
 
 recorder()
 text = transcribe()
 textTranslated = translater(text)
-
-print(textTranslated)
+text_to_speech(textTranslated.text)
---
+++ src/text_to_speech.py
@@ -0,0 +1,108 @@
+from gtts import gTTS
+
+LANG_CODE = 'en'
+
+def text_to_speech(text):
+    try:
+        """
+        #If you to read from file:
+        #Read the text from the TXT file
+        with open(txt, 'r', encoding='utf-8') as f:
+            text = f.read()
+        """
+        #Create a gTTS instance with the desired language
+        tts = gTTS(text=text, lang=LANG_CODE)
+
+        # Save the audio file as an MP3 file
+        tts.save('speech.mp3')
+    except Exception as e:
+        print(f'Error: {e}')
+
+"""
+ALL LANGUAGE CODES in GTTS
+
+Language	Code
+Afrikaans	af
+Albanian	sq
+Amharic	am
+Arabic	ar
+Armenian	hy
+Azerbaijani	az
+Basque	eu
+Belarusian	be
+Bengali	bn
+Bosnian	bs
+Bulgarian	bg
+Catalan	ca
+Cebuano	ce
+Chinese (Simplified)	zh-CN
+Chinese (Traditional)	zh-TW
+Croatian	hr
+Czech	cs
+Danish	da
+Dutch	nl
+English	en
+Esperanto	eo
+Estonian	et
+Finnish	fi
+French	fr
+Galician	gl
+Georgian	ka
+German	de
+Greek	el
+Gujarati	gu
+Haitian Creole	ht
+Hebrew	iw
+Hindi	hi
+Hungarian	hu
+Icelandic	is
+Indonesian	id
+Irish	ga
+Italian	it
+Japanese	ja
+Javanese	jv
+Khmer	km
+Kannada	kn
+Korean	ko
+Kurdish	ku
+Kyrgyz	ky
+Lao	lo
+Latvian	lv
+Lithuanian	lt
+Macedonian	mk
+Malayalam	ml
+Marathi	mr
+Malay	ms
+Maltese	mt
+Mongolian	mn
+Myanmar (Burmese)	my
+Nepali	ne
+Norwegian	no
+Pashto	ps
+Persian	fa
+Polish	pl
+Portuguese	pt
+Romanian	ro
+Russian	ru
+Samoan	sm
+Serbian	sr
+Slovak	sk
+Slovenian	sl
+Somali	so
+Spanish	es
+Sundanese	su
+Swahili	sw
+Swedish	sv
+Tagalog (Filipino)	tl
+Tamil	ta
+Telugu	te
+Thai	th
+Turkish	tr
+Ukrainian	uk
+Urdu	ur
+Uyghur	ug
+Vietnamese	vi
+Welsh	cy
+Yiddish	yi
+Zulu	zu
+"""
